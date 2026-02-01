4c6baedea4dfe74c00679c66d82992d43715e4c8
liubinrui <2705410026@qq.com>
2026-02-02 07:03:36 +0800

    一、初始化项目

---
+++ src/main/java/com/liubinrui/LldatiApplication.java
@@ -0,0 +1,13 @@
+package com.liubinrui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LldatiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LldatiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liubinrui/LldatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liubinrui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LldatiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
